d93cb1dfdbc39a5476a152e2e165161dccb40cc2
xuyichang <xuyichang@dajiazhongyi.com>
2018-02-09 15:44:21 +0800

    median of two sorted arrays, i.e, find kth element in two sorted arrays

---
+++ median_of_two_sorted_arrays/median_of_two_sorted_arrays.py
@@ -0,0 +1,49 @@
+# There are two sorted arrays nums1 and nums2 of size m and n respectively.
+#
+# Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+# Example 1:
+# nums1 = [1, 3]
+# nums2 = [2]
+# The median is 2.0
+
+# Example 2:
+# nums1 = [1, 2]
+# nums2 = [3, 4]
+# The median is (2 + 3)/2 = 2.5
+
+
+class Solution(object):
+    def find_median_sorted_arrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        total_len = len(nums1) + len(nums2)
+        if 1 & total_len:
+            return self.find_k(nums1, nums2, total_len / 2 + 1)
+        else:
+            return (self.find_k(nums1, nums2, total_len / 2) + self.find_k(nums1, nums2, total_len / 2 + 1)) / 2.0
+
+    def find_k(self, A, B, k):
+        la, lb, pa, pb = len(A), len(B), min(k / 2, len(A)), k - min(k / 2, len(A))
+        if la > lb:
+            return self.find_k(B, A, k)
+        if la == 0:
+            return B[k - 1]
+        if k == 1:
+            return min(A[0], B[0])
+        if A[pa - 1] < B[pb - 1]:
+            return self.find_k(A[pa:], B, k - pa)
+        elif A[pa - 1] > B[pb - 1]:
+            return self.find_k(A, B[pb:], k - pb)
+        else:
+            return A[pa - 1]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums1 = [1, 2]
+    nums2 = [3, 4]
+    print solution.find_median_sorted_arrays(nums1, nums2)
